b3c6484c9cbe430299bd9b111c39e0a6a641f988
woo <ldw28517@naver.com>
2023-06-27 01:13:32 +0900

    update

--- src/Recursion/백준2447_별찍기10/Main.java
+++ src/Recursion/백준2447_별찍기10/Main.java
@@ -23,23 +23,21 @@ public class Main {
     }
 
     private static void star(int n, char[][] paint, int x, int y) {
-
         if(n < 3) return;
 
-        int nextSize = n/3;
-        for(int i = nextSize; i < 2*nextSize; i++) {
-            for(int j = nextSize; j < 2*nextSize; j++) {
+        for(int i = n/3; i < 2 * n/3; i++) {
+            for(int j = n/3; j < 2 * n/3; j++) {
                 paint[i+x][j+y] = ' ';
             }
         }
-        star(nextSize, paint, x,y);
-        star(nextSize, paint, x + nextSize,y);
-        star(nextSize, paint,x + 2*nextSize,y);
-        star(nextSize, paint,x,y + nextSize);
-        star(nextSize, paint,x + 2*nextSize, y + nextSize);
-        star(nextSize, paint,x, y + 2*nextSize);
-        star(nextSize, paint,x + n/3, y + 2*nextSize);
-        star(nextSize, paint,x + 2*nextSize, y + 2*nextSize);
+        star(n/3, paint, x,y);
+        star(n/3, paint, x + n/3,y);
+        star(n/3, paint,x + 2*n/3,y);
+        star(n/3, paint,x,y + n/3);
+        star(n/3, paint,x + 2*n/3, y + n/3);
+        star(n/3, paint,x, y + 2*n/3);
+        star(n/3, paint,x + n/3, y + 2*n/3);
+        star(n/3, paint,x + 2*n/3, y + 2*n/3);
     }
 
 }
