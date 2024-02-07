38b70c1f379d1303b9225c0a97c7c5b4a4a7b6ac
woo <ldw28517@naver.com>
2024-02-07 21:09:59 +0900

    2024 카카오 인턴십 3번 문제풀이 업로드

--- src/Kakao/_2024/q3/Solution.java
+++ src/Kakao/_2024/q3/Solution.java
@@ -2,104 +2,94 @@ package Kakao._2024.q3;
 
 import java.util.*;
 class Solution {
-    static int n, maxWin;
-    static int[] resultPair;
-    static int[] diceA, diceB;
-    static List<Integer> sumA, sumB;
+    static int N, maxWin;
+    static int[] result, diceA, diceB;
+    static List<Integer> scoreA, scoreB;
+
 
     public int[] solution(int[][] dice) {
-        n = dice.length;
-        diceA = new int[n / 2];
-        diceB = new int[n / 2];
-        resultPair = new int[n / 2];
-        comb(0, 1, dice);
-        return resultPair;
+        N = dice.length;
+        result = new int[N / 2];
+        diceA = new int[N / 2];
+        diceB = new int[N / 2];
+        comb(0, 0, dice);
+        return result;
     }
 
-    static void comb(int cnt, int start, int[][] origin) {
-        if (cnt == n / 2) {
-            /**
-             * 1. 주사위 A 조합 구했으니 B 조합 나머지로 구해주기
-             * 2. A, B 각각 여러개의 주사위를 하나의 총점 배열로 만들어주기
-             * 3. 두 일차원 배열을 비교하면 승수를 구할 수 있음
-             */
+    static void comb(int start, int depth, int[][] origin) {
+        if (depth == N / 2) {
             findB();
-            sumA = new ArrayList<>();
-            sumB = new ArrayList<>();
-            makeSumArr(0, 0, origin, diceA, sumA);
-            makeSumArr(0, 0, origin, diceB, sumB);
+            scoreA = new ArrayList<>();
+            scoreB = new ArrayList<>();
+            findSum(0, 0, 0, diceA, scoreA, origin);
+            findSum(0, 0, 0, diceB, scoreB, origin);
             countWin();
             return;
         }
 
-        for (int i = start; i <= n; i++) {
-            diceA[cnt] = i;
-            comb(cnt + 1, i + 1, origin);
-        }
-    }
-
-    static void findB() {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            list.add(i);
-        }
-        for (int n : diceA) {
-            list.remove((Integer) n);
-        }
-        for (int i = 0; i < list.size(); i++) {
-            diceB[i] = list.get(i);
-        }
-    }
-
-    static void makeSumArr(int cnt, int sum, int[][] origin, int[] diceX, List<Integer> sumX) {
-        if (cnt == n / 2) {
-            sumX.add(sum);
-            return;
-        }
-
-        for (int i = 0; i < 6; i++) {
-            makeSumArr(cnt + 1, sum + origin[diceX[cnt] - 1][i], origin, diceX, sumX);
+        for (int i = start; i < N; i++) {
+            diceA[depth] = i;
+            comb(i + 1, depth + 1, origin);
         }
     }
 
     static void countWin() {
-        Collections.sort(sumA);
-        Collections.sort(sumB);
+        Collections.sort(scoreA);
+        Collections.sort(scoreB);
 
-        int result = 0;
-        int beforeN = 0;
-        int beforeCnt = 0;
+        int beforeWin = 0;
+        int winCnt = 0;
+        int beforeIdx = 0;
 
-        /**
-        * Integer 끼리 == 연산하면 일정 상수 범위를 넘어갔을 때
-        * 같은 상수값이어도 주소가 다르니 다르다고 판단.
-        * Integer <-> int 까지는 상관없다.
-        * -> 웬만하면 int 형으로 비교하자.
-        */
-        for(int num : sumA) {
-            if (num == beforeN) {
-                result += beforeCnt;
-                continue;
-            }
-            beforeN = num;
-            if (sumB.get(sumB.size()-1) < num) {
-                result += sumB.size();
-                beforeCnt = sumB.size();
-                continue;
-            }
-            for (int i = 0; i < sumB.size(); i++) {
-                if (sumB.get(i) == num || sumB.get(i) > num) {
-                    result += i;
-                    beforeCnt = i;
+        for (int i = 0; i < scoreA.size(); i++) {
+            winCnt += beforeWin;
+            for (int j = beforeIdx; j < scoreB.size(); j++) {
+                if (scoreA.get(i) > scoreB.get(j)) {
+                    beforeWin++;
+                    winCnt++;
+                    beforeIdx++;
+                }
+                else {
+                    beforeIdx = j;
                     break;
                 }
             }
         }
-        if (result > maxWin) {
-            maxWin = result;
-            for (int i = 0; i < diceA.length; i++) {
-                resultPair[i] = diceA[i];
+        System.out.println(winCnt);
+        if (winCnt > maxWin) {
+            maxWin = winCnt;
+            for (int i = 0; i < N / 2; i++) {
+                result[i] = diceA[i] + 1;
             }
+        }
+    }
+
+    static void findSum(int start, int depth, int sum, int[] dice, List<Integer> score, int[][] origin) {
+        if (depth == N / 2) {
+            score.add(sum);
+            return;
+        }
+
+        for (int i = start; i < 6; i++) {
+            sum += origin[dice[depth]][i];
+            findSum(start, depth + 1, sum, dice, score, origin);
+            sum -= origin[dice[depth]][i];
+        }
+    }
+
+
+    static void findB() {
+        List<Integer> tmp = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            tmp.add(i);
+        }
+
+        for (int n : diceA) {
+            tmp.remove((Integer) n);
+        }
+
+        for (int i = 0; i < N / 2; i++) {
+            diceB[i] = tmp.get(i);
         }
     }
 }
