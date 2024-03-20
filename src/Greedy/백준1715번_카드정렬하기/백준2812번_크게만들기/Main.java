18adba05531f22998c9ebb278f0a0d0a0b3f8e51
woo <ldw28517@naver.com>
2024-03-20 23:14:41 +0900

    백준 크게 만들기 문제풀이 업로드 (그리디 + 덱)

---
+++ src/Greedy/백준1715번_카드정렬하기/백준2812번_크게만들기/Main.java
@@ -0,0 +1,52 @@
+package Greedy.백준1715번_카드정렬하기.백준2812번_크게만들기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, K;
+    static Deque<Integer> dq;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        dq = new ArrayDeque<>();
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        String nums = br.readLine();
+
+        for (int i = 0; i < nums.length(); i++) {
+            while (true) {
+                if (dq.size() == 0) {
+                    dq.push(nums.charAt(i) - '0');
+                    break;
+                }
+
+                if (dq.peek() < nums.charAt(i) - '0' && K > 0) {
+                    dq.pop();
+                    K--;
+                } else {
+                    dq.push(nums.charAt(i) - '0');
+                    break;
+                }
+            }
+        }
+
+        for (int i = 1; i <= K; i++) {
+            dq.pop();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!dq.isEmpty()) {
+            sb.append(dq.removeLast());
+        }
+
+        System.out.println(sb.toString());
+    }
+}
