ef0fb183b07bbb35a4fff82dba864db23bf10b24
woo <ldw28517@naver.com>
2024-03-22 00:52:45 +0900

    백준 2Xn 타일링2 문제풀이 업로드 (dp)

---
+++ src/DP/백준11727번_2XN타일링2/Main.java
@@ -0,0 +1,27 @@
+package DP.백준11727번_2XN타일링2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dp = new int[1001];
+
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 2] * 2 + 2 + dp[i - 1] - 2) % 10007;
+        }
+
+        System.out.println(dp[N]);
+
+    }
+}
