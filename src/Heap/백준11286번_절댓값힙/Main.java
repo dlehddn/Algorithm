157a5c072af6aca7f7239537d1717cd31059184b
woo <ldw28517@naver.com>
2024-06-02 22:52:07 +0900

    백준 11286번 문제풀이 업로드

---
+++ src/Heap/백준11286번_절댓값힙/Main.java
@@ -0,0 +1,52 @@
+package Heap.백준11286번_절댓값힙;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    static PriorityQueue<Node> pq = new PriorityQueue<>();
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int command = Integer.parseInt(br.readLine());
+            if (command == 0) {
+                if (pq.size() != 0) {
+                    sb.append(pq.poll().num + "\n");
+                } else {
+                    sb.append("0" + "\n");
+                }
+            } else {
+                pq.add(new Node(command));
+            }
+        }
+
+        System.out.println(sb);
+
+
+    }
+
+    static class Node implements Comparable<Node>{
+        int num;
+
+        public Node(int num) {
+            this.num = num;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if (Math.abs(this.num) != Math.abs(o.num)) {
+                return Math.abs(this.num) - Math.abs(o.num);
+            } else {
+                return this.num - o.num;
+            }
+        }
+    }
+}
