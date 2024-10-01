7304964f98b06557bfddf47139f13ffddeed62a4
woo <ldw28517@naver.com>
2024-10-01 23:58:03 +0900

    백준2631번 줄세우기 문제풀이 업로드

---
+++ src/DP/백준2631번_줄세우기/Main2.java
@@ -0,0 +1,35 @@
+package DP.백준2631번_줄세우기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main2 {
+
+    static int N;
+    static int[] arr, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int max = Arrays.stream(dp).max().orElse(0);
+        System.out.println(N - max);
+    }
+}
