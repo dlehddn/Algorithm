9c4d846e93d1f10be1c03018d381a0e74109f03c
woo <ldw28517@naver.com>
2025-10-06 01:24:28 +0900

    백준 문제풀이 업로드

--- src/Greedy/백준1541번_잃어버린괄호/Main.java
+++ src/Greedy/백준1541번_잃어버린괄호/Main.java
@@ -4,18 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// start 12:20
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
-        int startIdx = 0;
-        int sum = 0;
+        StringBuilder sb = new StringBuilder();
+        char prevOps = '+';
+        int total = 0;
+        int pSum = 0;
 
-
-        for (int i = 1; i < input.length(); i++) {
-            if (input.charAt())
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                sb.append(input.charAt(i));
+                if (i != input.length() - 1) {
+                    continue;
+                }
+            }
+            int amount = Integer.parseInt(sb.toString());
+            if (i == input.length() - 1 || input.charAt(i) == '-') {
+                pSum += amount;
+                if (prevOps == '+') {
+                    total += pSum;
+                } else {
+                    total -= pSum;
+                }
+                pSum = 0;
+                prevOps = '-';
+            } else {
+                pSum += amount;
+            }
+            sb = new StringBuilder();
         }
+        System.out.println(total);
     }
 }
